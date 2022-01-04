2e51a8936286a3be03261c8d0d3f060f3cd21b9f
Faithful Ojebiyi <faithfulojebiyi@gmail.com>
2022-01-04 07:51:16 +0100

    Ft: (Fix docker microservice config) )

--- category/app/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const category = require('./category')
-const template = require('./template')
-
-const router = Router()
-
-router.use('/category', category)
-router.use('/template', template)
-module.exports = router
